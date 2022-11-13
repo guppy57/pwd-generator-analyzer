2aa1499f02e4061b5adb36eb0a34b3ca1216186d
Armaan Gupta <armaan@guppy.im>
2022-11-13 15:30:21 -0600

    Added chakra utils and themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({ 
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  },
+  fonts: {
+    heading: `'CraftworkSans-Bold', sans-serif`,
+    body: `'CraftworkSans-Regular', sans-serif`
+  }
+})
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
